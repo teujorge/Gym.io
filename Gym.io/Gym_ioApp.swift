e0c16de420d7657b83cd4c1767e8f6bfcace202a
Matheus Jorge <mrljorge@outlook.com>
2024-07-04 09:30:24 -0300

    Initial Commit

---
+++ Gym.io/Gym_ioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_ioApp.swift
+//  Gym.io
+//
+//  Created by Matheus Jorge on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Gym_ioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
